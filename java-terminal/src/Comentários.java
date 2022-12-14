44e29e8caa7a27c7874210f420b97056751288de
AndréHugoFernandes <andrehugofernandes@gmail.com>
2022-12-14 12:46:19 -0300

    Esse projeto é de documentação, infelizmente coloquei na pasta errada

---
+++ java-terminal/src/Comentários.java
@@ -0,0 +1,25 @@
+public class Comentários {
+  public static void main(String[] args) {
+
+}
+/*
+ * Este método foi elaborado as pressas
+ * por isso eu abrevei o nome das variáveisE
+ * mas olha, ele tem a finalidade somar ou  multiplicar
+ * dois números
+ */
+ 
+
+public int somaMultiplica (int n, int x, String m){
+
+  
+    int r = 0; // r é igual ao resultado
+    if (m == "M"){ // M= multiplicação
+        r= n * x;
+    }else{
+        // se não soma mesmo
+        r = n + x;
+    }
+    return r;
+}
+}
