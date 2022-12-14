d01c9535db7a615587fa56db6f5f1e42de03a283
AndréHugoFernandes <andrehugofernandes@gmail.com>
2022-12-14 11:07:21 -0300

    Logic Operators

---
+++ tipos-variaveis/src/OperadoresRelacionais.java
@@ -0,0 +1,27 @@
+public class OperadoresRelacionais {
+  public static void main(String[] args) {
+    // classe Operadores.java
+    int numero1 = 1;
+    int numero2 = 2;
+
+    if (numero1 > numero2) {
+      System.out.print("Numero 1 maior que numero 2");
+    }
+
+    if (numero1 < numero2) {
+      System.out.print("Numero 1 menor que numero 2");
+    }
+
+    if (numero1 >= numero2) {
+      System.out.print("Numero 1 maior ou igual que numero 2");
+    }
+
+    if (numero1 <= numero2) {
+      System.out.print("Numero 1 menor ou igual que numero 2");
+    }
+
+    if (numero1 != numero2) {
+      System.out.print("Numero 1 é diferente de numero 2");
+    }
+  }
+}
