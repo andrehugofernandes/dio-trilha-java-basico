fba44095af9b8fd37a670477791a19415508dea5
AndréHugoFernandes <andrehugofernandes@gmail.com>
2022-12-14 09:46:51 -0300

    Concat Operators learning

---
+++ tipos-variaveis/src/Operadores.java
@@ -0,0 +1,25 @@
+public class Operadores {
+
+  public static void main(String[] args) {
+    String nomeCompleto = "LINGUAGEM" + "JAVA";
+    System.out.println(nomeCompleto);
+
+    // qual o resultado das expressoes abaixo?
+    String concatenacao = "?";
+
+    concatenacao = 1 + 1 + 1 + "1";
+    System.out.println(concatenacao);
+
+    concatenacao = 1 + "1" + 1 + 1;
+    System.out.println(concatenacao);
+
+    concatenacao = 1 + "1" + 1 + "1";
+    System.out.println(concatenacao);
+
+    concatenacao = "1" + 1 + 1 + 1;
+    System.out.println(concatenacao);
+
+    concatenacao = "1" + (1 + 1 + 1);
+    System.out.println(concatenacao);
+  }
+}
