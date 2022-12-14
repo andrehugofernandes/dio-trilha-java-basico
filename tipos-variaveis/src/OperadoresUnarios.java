45e7d3dc7b034c9a7b6af12d21bee7dc20b915d8
AndréHugoFernandes <andrehugofernandes@gmail.com>
2022-12-14 10:13:01 -0300

    Single operators

---
+++ tipos-variaveis/src/OperadoresUnarios.java
@@ -0,0 +1,27 @@
+public class OperadoresUnarios {
+
+  public static void main(String[] args) {
+    // classe Operadores.java
+    int numero = 5;
+
+    // Imprimindo o numero negativo
+    System.out.println(-numero);
+
+    // incrementando numero em mais 1 numero, imprime 6
+    numero++;
+    System.out.println(numero);
+
+    // incrementando numero em mais 1 numero, imprime 7
+    System.out.println(numero++);// ops algo de errado não está certo
+
+    System.out.println(numero);// agora sim, numero virou 7
+
+    // ordem de precedencia conta aqui
+    System.out.println(++numero);
+
+    boolean verdadeiro = true;
+
+    System.out.println("Inverteu " + !verdadeiro);
+  }
+
+}
